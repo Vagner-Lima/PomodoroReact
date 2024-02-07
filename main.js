6db0db8f2f0282dc04426125e0e7505624dd9e26
Lim <145813658+Vagner-Lima@users.noreply.github.com>
2024-02-07 09:49:37 -0300

    Base do programa
    ainda falta fazer o timer funcionar

---
+++ main.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('pomodoroTimer')
+function startTimer(duration){
+    var timer = duration, minutes, seconds
+    setInterval(() => {
+        
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer <0){
+            timer = duration;
+        }
+    }, 1000);
+}
